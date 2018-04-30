b1c8438a00854c5450ef89254eb428669edf2d1b
Ionut Craciunescu <icraciunescu@gmail.com>
2018-04-30 13:37:00 +0100

    do not use the Xwindows backend.

--- src/anonymise.py
+++ src/anonymise.py
@@ -13,7 +13,7 @@ import numpy as np
 from skimage.transform import radon
 from skimage.morphology import disk, closing
 import matplotlib as mpl
-mpl.use('TkAgg')
+mpl.use('Agg')
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
 from pycorenlp import StanfordCoreNLP
